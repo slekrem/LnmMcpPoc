57966362a94028a00b714cfbf68ff3a28d70fa94
Stefan <lekrem@icloud.com>
2025-06-08 14:01:25 +0200

    Update Program.cs

--- LnmMcpPoc/Program.cs
+++ LnmMcpPoc/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System.Reflection;
 
 var builder = Host.CreateApplicationBuilder(args);
